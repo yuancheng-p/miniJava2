c799b3678889f44f1523a9658982933bc2ddd5ac
Yuancheng Peng <yuancheng.peng@telecom-bretagne.eu>
2016-01-30 02:56:13 +0100

    build_global_env: step 1
    * only add class names into its env

--- Typing/Typing.ml
+++ Typing/Typing.ml
@@ -2,47 +2,56 @@ open AST
 open Type
 
 
-let type_single_ast ast =
-  (match ast.info with
-   | Class c -> print_endline "class found"
-   | Inter -> print_endline "inner part")
-
 type class_env = {
   parent: ref_type;
   methods: string list; (* TODO replace me by methods list*)
   attributes: string list; (* TODO replace me by attributes list *)
 }
 
+
+(* TODO add package *)
+let rec build_classes_names classes_env asts =
+  match asts with
+  | [] -> classes_env
+  | h::others -> (match h.info with
+      | Class c ->
+        (Env.add classes_env
+            {tpath = []; tid = h.id}
+            {parent = c.cparent; methods = []; attributes = []});
+        build_classes_names classes_env others
+      | Inter -> classes_env
+  )
+
+
+let str_of_ref_type ref_type = ref_type.tid
+
+
+let str_of_class_env class_env = "class_env(parent="^class_env.parent.tid^")\n"
+
+
+let print_classes_env classes_env =
+  Env.iter (
+    fun (r_type, c_env) ->
+      print_string
+      (str_of_ref_type r_type ^ " : " ^ str_of_class_env c_env);
+    ) classes_env
+
+
 let build_global_env ast =
   (* Key: ref_type; Value: class_env *)
   let classes_env = Env.initial();
-
   in
-
-  (*each class has its own env*)
-
+  (* load predefined classes, each class has its own class_env *)
   List.iter
     (fun (r_type, cls_env) ->
       Env.add classes_env r_type cls_env; ())
-    [ {tpath=[]; tid="Object"}, {
-        parent = {tpath=[]; tid=""};
-        methods = []; attributes = [];
-      };
-      {tpath=[]; tid="Integer"}, {
-        parent = Type.object_type;
-        methods = []; attributes = [];
-      };
-    ];
-
-
-  (* TODO: delete me, this code is just for testing ...*)
-  let int_cls_env = Env.find classes_env {tpath=[]; tid="Integer"};
-  in
-  print_string "the parent of Integer is:";
-  print_endline int_cls_env.parent.tid;
-
-  (* TODO: delete me, just for testing ...*)
-  List.iter (fun tree -> type_single_ast tree;) ast.type_list
+    [ { tpath=[]; tid="Object" }, { parent = {tpath=[]; tid=""}; methods = []; attributes = []; };
+      { tpath=[]; tid="Integer" }, { parent = Type.object_type; methods = []; attributes = []; };
+      { tpath=[]; tid="String" }, { parent = Type.object_type; methods = []; attributes = []; };
+      { tpath=[]; tid="Boolean" }, { parent = Type.object_type; methods = []; attributes = []; }; ];
+  (*step 1*)
+  build_classes_names classes_env ast.type_list;
+  print_classes_env classes_env
 
 
 let typing ast =
