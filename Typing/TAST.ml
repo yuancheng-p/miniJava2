d550049339726f60492dca70f4c6eae6c4b37787
Yuancheng Peng <yuancheng.peng@telecom-bretagne.eu>
2016-02-08 00:48:23 +0100

    reconstruct the TAST
    * turn AST type into TAST type.
    Each AST node reflect to a TAST node.
    
    * no type is added yet
    * no check is added

--- Typing/TAST.ml
+++ Typing/TAST.ml
@@ -1,3 +1,10 @@
+type t_argument = {
+    t_final : bool;
+    t_vararg : bool;
+    t_ptype : Type.t;
+    t_pident : string;
+  }
+
 type t_value =
   | TString of string
   | TInt of int
@@ -8,40 +15,97 @@ type t_value =
 
 type t_expression_desc =
   | TVal of t_value
+  | TName of string
+  | TAssignExp of t_expression * AST.assign_op * t_expression
+  | TNew of string option * string list * t_expression list
+  | TNewArray of Type.t * (t_expression option) list * t_expression option
+  | TCall of t_expression option * string * t_expression list
+  | TAttr of t_expression * string
+  | TIf of t_expression * t_expression * t_expression
+  | TArrayInit of t_expression list
+  | TArray of t_expression * (t_expression option) list
+  | TPost of t_expression * AST.postfix_op
+  | TPre of AST.prefix_op * t_expression
+  | TOp of t_expression * AST.infix_op * t_expression
+  | TCondOp of t_expression * t_expression * t_expression
+  | TCast of Type.t * t_expression
+  | TType of Type.t
+  | TClassOf of Type.t
+  | TInstanceof of t_expression * Type.t
+  | TVoidClass
+  | TQN of string list
 
-type t_expression = {
-      t_edesc : t_expression_desc;
+and t_expression = {
+    t_edesc : t_expression_desc;
+    (*TODO should we add a type?: t_type : Type.t; *)
+(*   t_eloc : Location.t;
+     mutable t_etype : Type.t option;*)
 }
+
+type t_astattribute = {
+      mutable amodifiers : AST.modifier list;
+      t_aname : string;
+      t_atype : Type.t;
+      t_adefault : t_expression option;
+      (*      aloc : Location.t;*)
+    }
 
 type t_statement =
   | TExpr of t_expression
+  | TVarDecl of (Type.t * string * t_expression option) list
+  | TBlock of t_statement list
+  | TNop
+  | TWhile of t_expression * t_statement
+  | TFor of (Type.t option * string * t_expression option) list * t_expression option * t_expression list * t_statement
+  | TIf of t_expression * t_statement * t_statement option
+  | TReturn of t_expression option
+  | TThrow of t_expression
+  | TTry of t_statement list * (t_argument * t_statement list) list * t_statement list
 
 type t_astmethod = {
-
     t_mbody : t_statement list;
-    t_mreturntype : Type.t;
-
-  (*
-    mutable t_mmodifiers : modifier list;
+    t_mreturntype : Type.t; (* TODO: what should we do? *)
+(* TODO  mutable t_mmodifiers : AST.modifier list;
     t_mname : string;
-    t_margstype : argument list;
-    t_mthrows : Type.ref_type list;
-    *)
+    t_margstype : t_argument list;
+    t_mthrows : Type.ref_type list; *)
 }
+
+type t_astconst = {
+    mutable t_cmodifiers : AST.modifier list;
+    t_cname : string;
+    t_cargstype : t_argument list;
+    t_cthrows : Type.ref_type list;
+    t_cbody : t_statement list;
+    (*      t_mloc : Location.t;*)
+  }
 
 type t_astclass = {
     t_cmethods : t_astmethod list;
-
-    (*
-    t_cparent : Type.ref_type;
-    t_cattributes : astattribute list;
-    t_cinits : initial list;
-    t_cconsts : astconst list;
-    t_ctypes : asttype list;
+(* TODO  t_cparent : Type.ref_type;
+    t_cattributes : t_astattribute list;
+    t_cinits : t_initial list;
+    t_cconsts : t_astconst list;
+    t_ctypes : t_asttype list;
     t_cloc : Location.t; *)
   }
 
-
-type typed_class_or_expr =
+type t_type_info =
   | TClass of t_astclass
+  | TInter
 
+type t_initial = {
+    t_static : bool ;
+    t_block : t_statement list
+  }
+
+and t_asttype = {
+    mutable t_modifiers : AST.modifier list;
+    t_id : string;
+    t_info : t_type_info;
+  }
+
+type t_t = {
+    t_package : AST.qualified_name option;
+    t_type_list : t_asttype list;
+  }
--- Typing/Typing.ml
+++ Typing/Typing.ml
@@ -1,15 +1,7 @@
 open AST
 open Type
 open TAST
-open EnvType
 open Helper
-
-(* This function checks that a type exists for the given classname, and returns it. *)
-let type_of_method_signature methods key =
-  (* TODO raise error if not found *)
-  let v = Env.find methods key
-    in v.return_type
-
 
 let type_value v =
   match v with
@@ -21,18 +13,39 @@ let type_value v =
   | AST.Null -> TNull
 
 
-let type_exprssion_desc edesc =
+let type_expression_desc env edesc =
   match edesc with
   | Val v -> TVal(type_value v)
+  (* TODO: check and type all the expressions here *)
+
+
+let type_expression env e =
+  {
+    t_edesc = type_expression_desc env e.edesc;
+  }
+
+
+let rec type_var_decl_list env vd_list =
+  let type_var_decl env vd =
+    (* TODO: add vars into env *)
+    match vd with
+    | (t, s, Some e) -> (t, s, Some (type_expression env e))
+    | (t, s, None) -> (t, s, None)
+  in match vd_list with
+  | [] -> []
+  | h::others -> type_var_decl env h::(type_var_decl_list env others)
 
 
 let rec type_statement_list env l =
-  let type_stmt stmt =
+  let type_statment stmt =
     match stmt with
-    | Expr e -> TExpr({t_edesc=(type_exprssion_desc e.edesc)})
+    | VarDecl vd_list -> TVarDecl(type_var_decl_list env vd_list)
+    | Expr e -> TExpr(type_expression env e)
+    (*TODO: check and type all the statments here *)
+
   in match l with
     | [] -> []
-    | t::q -> (type_stmt t)::(type_statement_list env q)
+    | t::q -> (type_statment t)::(type_statement_list env q)
 
 
 let rec type_method_list env class_curr l =
@@ -40,6 +53,7 @@ let rec type_method_list env class_curr l =
     {
       t_mbody = (type_statement_list env m.mbody);
       t_mreturntype = m.mreturntype
+      (* TODO: t_mname, t_margstype, t_mthrows *)
     }
   in match l with
      | [] -> []
@@ -48,19 +62,32 @@ let rec type_method_list env class_curr l =
 
 let typing ast verbose =
   let env = GlobalEnv.build_global_env ast verbose
-  in let rec type_rec_structure_tree_list tree_list =
-    (* This inner function receives a non-located class_or_expr *)
-    let type_structure t =
-      let ref_type={tpath = (trim_option_type ast.package []); tid = t.id}
-      in let cur_cls = Env.find env ref_type
-      in match t.info with
-      | Class c ->
-          TClass({
-            t_cmethods = (type_method_list env cur_cls c.cmethods)
-          })
-    in match tree_list with
-    | [] -> []
-    | t::q -> type_structure t::(type_rec_structure_tree_list q)
-  in
-  type_rec_structure_tree_list ast.type_list
 
+  (* for each AST, turn it into TAST *)
+  in let rec type_type_list type_list =
+
+    let type_asttype asttype =
+
+      let type_type_info t =
+        let ref_type = {tpath = []; tid = t.id}
+        in let cur_cls = Env.find env ref_type
+        in match t.info with
+        | Class c ->
+            TClass({
+              t_cmethods = (type_method_list env cur_cls c.cmethods)
+            })
+
+      in {
+        t_modifiers = asttype.modifiers;
+        t_id = asttype.id;
+        t_info = type_type_info asttype;
+      }
+
+    in match type_list with
+    | [] -> []
+    | h::others -> type_asttype h::(type_type_list others)
+
+  in {
+    t_package = ast.package;
+    t_type_list = type_type_list ast.type_list
+  }
