ae015bc942e7389bad2b99064c45405aac9662ef
Yuancheng Peng <yuancheng.peng@telecom-bretagne.eu>
2016-02-28 17:43:38 +0100

    support TAttr & "this" in the functions
    * test case 1:
    this.val = val;
    
    * test case 2:
    
    Node n = new Node();
    n.next = new Node();
    n.next.next = new Node();
    n.next.val = 999;

---
+++ tests/Eval/TAttr.java
@@ -0,0 +1,35 @@
+public class Main {
+
+  public static void main () {
+    Person p = new Person();
+    p.setAge(12);
+    int a = p.getAge();
+    int k = 1;
+    Node n1 = new Node();
+    n1.val = 888;
+    n1.next = new Node();
+    n1.next.val = 999;
+    n1.next.next = new Node();
+    n1.next.next.val = 111;
+  }
+}
+
+
+class Person {
+
+  int age;
+
+  void setAge(int a) {
+    this.age = a;
+  }
+
+  int getAge() {
+    return age;
+  }
+
+}
+
+class Node {
+  public int val;
+  public Node next;
+}
